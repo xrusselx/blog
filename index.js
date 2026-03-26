e43ce6b83bb240042cabb08f47c9c8b0c55a5e91
Russel <russelsaints@gmail.com>
2026-03-26 10:14:23 +0800

    Initial commit: setup blog project structure

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Set EJS as the template engine
+app.set("view engine", "ejs");
+
+// Serve static files from public
+app.use(express.static("public"));
+
+// Home route
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Russel Notes",
+    posts: [{ title: "One Piece", date: "03/26/2026" }],
+  });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
